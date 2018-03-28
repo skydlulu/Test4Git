b4c9adedd7164af9fedb4f2727f2a3ef0f8b5d7c
skydlulu <skydlulu@skydlulu-PC>
2018-03-29 00:16:20 +0800

    test

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -2,15 +2,6 @@ package com.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hello kitty");
-		System.out.println("test hello");
-		System.out.println("git edit");
-		System.out.println("git edit2");
-		System.out.println("git edit3");
-        System.out.println("git hub edit");
-        System.out.println("git local edit");
-
-		System.out.println("git hub edit2");
+		System.out.println("localhost");
 	}
 }
